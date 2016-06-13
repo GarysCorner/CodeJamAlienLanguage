43f678f73b98ac04da3cddd51d51f8f3b92da8e7
Gary Andrew Bezet <Gary@GarysCorner.NET>
2016-06-13 10:18:30 -0500

    Initial commit

---
+++ codejamalienlanguage.go
@@ -0,0 +1,8 @@
+//File:		codejamalienlanguage.go
+//Author:	Gary Bezet
+//Date:		2016-06-13
+//Desc:		This program is designed to solve Google Code Jam "Alien Language"
+//Problem:	https://code.google.com/codejam/contest/90101/dashboard
+
+
+
